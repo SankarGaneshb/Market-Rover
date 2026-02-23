6064d8a7e705c88a077ca7a6c9af4928734f4b19
Sankar Ganesh <b.sankarganesh@gmail.com>
2026-02-23 18:25:47 +0530

    feat: complete InvestCraft navigation and routing integration

--- investcraft/frontend/src/App.js
+++ investcraft/frontend/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Puzzle from './pages/Puzzle';
 import Leaderboard from './pages/Leaderboard';
 import Profile from './pages/Profile';
+import Vote from './pages/Vote';
 
 function PrivateRoute({ children }) {
   const { user } = useAuth();
@@ -24,6 +25,7 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/play" element={<PrivateRoute><Puzzle /></PrivateRoute>} />
             <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/vote" element={<PrivateRoute><Vote /></PrivateRoute>} />
             <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
           </Routes>
         </BrowserRouter>
